33e963aacb63af13f5d7c147eaa16a7d87f48520
RajithAshok <rajithashok1@gmail.com>
2023-08-19 02:16:39 +0530

    SideBar added

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import {SideBar} from "./SideBar";
+
+
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack sx={{flexDirection:{ sx: "column", md:"row"}}}>
+      <Box sx={{ height: {sx:'auto', md:'92vh'}, borderRight: '1px solid #3d3d3d', px: { sx:0, md:2}}}>
+        <SideBar />
+        <Typography className="copyright" varient="body2" sx={{mt:1.5, color: '#fff'}}>
+          Copyright 2023 Rajith Ashok
+        </Typography>
+      </Box>
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+          <span style={{ color: "#FC1503" }}>videos</span>
+        </Typography>
+
+      </Box>
+    </Stack>
   )
 }
 
